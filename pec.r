cc1efccc9c6e0a92fb159338537761dae1c8c19b
rgarciarui <rgarciarui@uoc.edu>
2019-01-21 15:06:07 +0100

    actualizacion

--- pec.r
+++ pec.r
@@ -1,3 +1,14 @@
 
 library(tidyverse)
-bikes = read_csv("bikes2016.csv")
+bikes <- read_csv("bikes2016.csv")
+str(bikes)
+
+bikes <- bikes[, -2]
+
+map(bikes, ~mean(is.na(.)))
+bikes %>% map(~ mean(is.na(.)))
+
+bikes %>% 
+  summarise_all(funs(100*mean(is.na(.))))
+
+attach(bikes)
