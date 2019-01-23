fc42428e849a36107e24d5c7465b646912d91527
rgarciarui <rgarciarui@uoc.edu>
2019-01-23 11:42:36 +0100

    actualización pregunta 3

--- Pec_2.r
+++ Pec_2.r
@@ -11,3 +11,14 @@ kable(cbind(titanic, titanic), caption = "Datos titanic.csv") %>%
   kable_styling() %>%
   scroll_box(width = "100%", height = "200px")
 
+
+titanic %>% 
+  select(Survived) %>% 
+  summarise(Sobrevivientes = 100*(sum(Survived)/n())) 
+
+
+
+titanic %>% 
+  summarise_all(funs(100*mean(is.na(.)))) %>% kable() %>%
+  kable_styling() %>%
+  scroll_box(width = "100%", height = "200px")
