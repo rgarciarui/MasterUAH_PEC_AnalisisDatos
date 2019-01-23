2255f575aac8df2e88a79828d623105810690777
rgarciarui <rgarciarui@uoc.edu>
2019-01-23 13:51:16 +0100

    actualizacion pregunta 5

--- Pec_2.r
+++ Pec_2.r
@@ -24,3 +24,16 @@ titanic %>%
 titanic <- dplyr::select(titanic, -Cabin)
 
 str(titanic)
+
+titanic %>%
+  separate(Name, c("apellido", "Nombre"), sep = ",") %>%
+  separate(Nombre, c("none", "Title"), sep = " ") %>% 
+  select(-none, -apellido) %>%
+  mutate(Title = ifelse(Title == 'Mlle.', 'Miss.', Title)) %>%
+  mutate(Title = ifelse(Title == 'Ms.|Mme.', 'Mrs.', Title)) %>%
+  mutate(Title = ifelse(!Title %in% c('Mr.', 'Mrs.', 'Master.', 'Miss.'), 'Otro', Title)) %>%
+  kable() %>%
+  kable_styling() %>%
+  scroll_box(width = "100%", height = "200px")
+
+
