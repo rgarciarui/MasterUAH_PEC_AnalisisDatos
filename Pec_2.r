5e641df33b493cb73a4731cdbba14192b1c196bf
rgarciarui <rgarciarui@uoc.edu>
2019-01-23 11:53:53 +0100

    actualizacion pregutna 4

--- Pec_2.r
+++ Pec_2.r
@@ -16,9 +16,11 @@ titanic %>%
   select(Survived) %>% 
   summarise(Sobrevivientes = 100*(sum(Survived)/n())) 
 
-
-
 titanic %>% 
   summarise_all(funs(100*mean(is.na(.)))) %>% kable() %>%
   kable_styling() %>%
   scroll_box(width = "100%", height = "200px")
+
+titanic <- dplyr::select(titanic, -Cabin)
+
+str(titanic)
